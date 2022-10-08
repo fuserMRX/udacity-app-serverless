38bf4b335030c35dafbb14ea9a37c0522d2e5f12
ast_vnovodid <v.novodid@astoundcommerce.com>
2022-10-08 10:38:30 +0300

    feat: add logic for signed urls

--- src/lambda/http/createImage.js
+++ src/lambda/http/createImage.js
@@ -1,12 +1,16 @@
-'use strict';
-
+import 'source-map-support/register';
 import * as AWS from 'aws-sdk';
 import * as uuid from 'uuid';
 
 const docClient = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3({
+    signatureVersion: 'v4'
+});
 
 const groupsTable = process.env.GROUPS_TABLE;
 const imagesTable = process.env.IMAGES_TABLE;
+const bucketName = process.env.IMAGES_S3_BUCKET;
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION;
 
 export const handler = async (event) => {
     console.log('Caller event', event)
@@ -28,16 +32,19 @@ export const handler = async (event) => {
     const imageId = uuid.v4();
     const newItem = await createImage(groupId, imageId, event);
 
+    const url = getUploadUrl(imageId);
+
     return {
         statusCode: 201,
         headers: {
             'Access-Control-Allow-Origin': '*'
         },
         body: JSON.stringify({
-            newItem: newItem
+            newItem: newItem,
+            uploadUrl: url
         })
     }
-};
+}
 
 async function groupExists(groupId) {
     const result = await docClient
@@ -51,18 +58,19 @@ async function groupExists(groupId) {
 
     console.log('Get group: ', result);
     return !!result.Item;
-};
+}
 
 async function createImage(groupId, imageId, event) {
-    const timestamp = new Date().toISOString()
-    const newImage = JSON.parse(event.body)
+    const timestamp = new Date().toISOString();
+    const newImage = JSON.parse(event.body);
 
     const newItem = {
         groupId,
         timestamp,
         imageId,
         ...newImage,
-    }
+        imageUrl: `https://${bucketName}.s3.amazonaws.com/${imageId}`
+    };
     console.log('Storing new item: ', newItem);
 
     await docClient
@@ -73,4 +81,12 @@ async function createImage(groupId, imageId, event) {
         .promise();
 
     return newItem;
-};
+}
+
+function getUploadUrl(imageId) {
+    return s3.getSignedUrl('putObject', {
+        Bucket: bucketName,
+        Key: imageId,
+        Expires: +urlExpiration
+    })
+}

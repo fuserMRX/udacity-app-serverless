e0926559661d8bc73badf942f95d0ecbd7ad6c52
ast_vnovodid <v.novodid@astoundcommerce.com>
2022-10-14 19:35:11 +0300

    feat: 'kibana text search' and sync between DynamoDB + ElasticSearch

--- src/lambda/dynamoDb/elasticSearchSync.js
+++ src/lambda/dynamoDb/elasticSearchSync.js
@@ -2,40 +2,40 @@ import 'source-map-support/register';
 import * as elasticsearch from 'elasticsearch';
 import * as httpAwsEs from 'http-aws-es';
 
-// const esHost = process.env.ES_ENDPOINT;
+const esHost = process.env.ES_ENDPOINT;
 
-// const es = new elasticsearch.Client({
-//   hosts: [ esHost ],
-//   connectionClass: httpAwsEs
-// });
+const es = new elasticsearch.Client({
+    hosts: [esHost],
+    connectionClass: httpAwsEs,
+});
 
 export const handler = async (event) => {
     console.log('Processing events batch from DynamoDB', JSON.stringify(event))
 
     for (const record of event.Records) {
         console.log('Processing record', JSON.stringify(record))
-        // if (record.eventName !== 'INSERT') {
-        //   continue
-        // }
+        if (record.eventName !== 'INSERT') {
+            continue;
+        }
 
-        // const newItem = record.dynamodb.NewImage
+        const newItem = record.dynamodb.NewImage;
 
-        // const imageId = newItem.imageId.S
+        const imageId = newItem.imageId.S;
 
-        // const body = {
-        //   imageId: newItem.imageId.S,
-        //   groupId: newItem.groupId.S,
-        //   imageUrl: newItem.imageUrl.S,
-        //   title: newItem.title.S,
-        //   timestamp: newItem.timestamp.S
-        // }
+        const body = {
+            imageId: newItem.imageId.S,
+            groupId: newItem.groupId.S,
+            imageUrl: newItem.imageUrl.S,
+            title: newItem.title.S,
+            timestamp: newItem.timestamp.S
+        }
 
-        // await es.index({
-        //   index: 'images-index',
-        //   type: 'images',
-        //   id: imageId,
-        //   body
-        // })
+        await es.index({
+            index: 'images-index',
+            type: 'images',
+            id: imageId,
+            body
+        });
 
     }
 };

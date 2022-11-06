17ba21b6c49a136361505864d661295815b4835f
ast_vnovodid <v.novodid@astoundcommerce.com>
2022-11-06 17:11:37 +0200

    feat: add express js approach in lambda functions - getGroups.ts spesifically

--- src/lambda/http/getGroups.ts
+++ src/lambda/http/getGroups.ts
@@ -1,19 +1,40 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import 'source-map-support/register'
+// import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+// import 'source-map-support/register'
 import { getAllGroups } from '../../businessLogic/groups';
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log('Processing event: ', event)
+import * as express from 'express'
+import * as awsServerlessExpress from 'aws-serverless-express'
 
-  const groups = await getAllGroups()
+const app = express();
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      items: groups
-    })
-  }
+app.get('/groups', async (req, res) => {
+  const groups = await getAllGroups();
+
+  res.json({
+    items: groups
+  });
+});
+
+// Old standard solution
+// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+//   console.log('Processing event: ', event)
+
+//   const groups = await getAllGroups()
+
+//   return {
+//     statusCode: 200,
+//     headers: {
+//       'Access-Control-Allow-Origin': '*'
+//     },
+//     body: JSON.stringify({
+//       items: groups
+//     })
+//   }
+// }
+
+// Create Express server
+const server = awsServerlessExpress.createServer(app);
+// Pass API Gateway events to the Express server
+exports.handler = (event, context) => {
+  awsServerlessExpress.proxy(server, event, context)
 }
